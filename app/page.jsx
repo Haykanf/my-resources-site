24eb4e359c6f782ca7ec7424c5d0aa1b925386e6
Haykanf <hay.knafo@gmail.com>
2026-05-10 00:56:12 +0300

    fix missing a tag

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default async function Home() {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {resources.map((resource) => (
-         
+         <a
             key={resource.id}
             href={resource.url}
             target="_blank"
